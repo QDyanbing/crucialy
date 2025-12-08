d9ee28031b871ca522d9e4e6f20e82e3c6342683
高艳兵 <yanbing.gao@aliyun.com>
2025-12-08 09:52:02 +0800

    refactor(stylelint): 简化 SCSS 配置代码
    - 移除导入语句前的分类注释
    - 简化 plugins 配置，使用类型断言替代复杂判断

--- packages/lint/src/stylelint/scss.ts
+++ packages/lint/src/stylelint/scss.ts
@@ -8,57 +8,31 @@
 import type { Config } from 'stylelint';
 import { base } from './base';
 
-// 按照官方文档分类顺序导入 SCSS 规则分组
-// @-each
 import { scssAtEachRules } from './scss-rules/at-each';
-// @-else
 import { scssAtElseRules } from './scss-rules/at-else';
-// @-extend
 import { scssAtExtendRules } from './scss-rules/at-extend';
-// @-function
 import { scssAtFunctionRules } from './scss-rules/at-function';
-// @-if
 import { scssAtIfRules } from './scss-rules/at-if';
-// @-import
 import { scssAtImportRules } from './scss-rules/at-import';
-// @-mixin
 import { scssAtMixinRules } from './scss-rules/at-mixin';
-// @-rule
 import { scssAtRuleRules } from './scss-rules/at-rule';
-// @-use
 import { scssAtUseRules } from './scss-rules/at-use';
-// $-variable
-import { scssDollarVariableRules } from './scss-rules/dollar-variable';
-// %-placeholder
-import { scssPercentPlaceholderRules } from './scss-rules/percent-placeholder';
-// //-comment
-import { scssDoubleSlashCommentRules } from './scss-rules/double-slash-comment';
-// Block
 import { scssBlockRules } from './scss-rules/block';
-// Comment
 import { scssCommentRules } from './scss-rules/comment';
-// Declaration
 import { scssDeclarationRules } from './scss-rules/declaration';
-// Dimension
 import { scssDimensionRules } from './scss-rules/dimension';
-// Function
+import { scssDollarVariableRules } from './scss-rules/dollar-variable';
+import { scssDoubleSlashCommentRules } from './scss-rules/double-slash-comment';
 import { scssFunctionRules } from './scss-rules/function';
-// Map
-import { scssMapRules } from './scss-rules/map';
-// Media feature
-import { scssMediaFeatureRules } from './scss-rules/media-feature';
-// Operator
-import { scssOperatorRules } from './scss-rules/operator';
-// Partial
-import { scssPartialRules } from './scss-rules/partial';
-// Property
-import { scssPropertyRules } from './scss-rules/property';
-// Selector
-import { scssSelectorRules } from './scss-rules/selector';
-// Load
-import { scssLoadRules } from './scss-rules/load';
-// General / Sheet
 import { scssGeneralRules } from './scss-rules/general';
+import { scssLoadRules } from './scss-rules/load';
+import { scssMapRules } from './scss-rules/map';
+import { scssMediaFeatureRules } from './scss-rules/media-feature';
+import { scssOperatorRules } from './scss-rules/operator';
+import { scssPartialRules } from './scss-rules/partial';
+import { scssPercentPlaceholderRules } from './scss-rules/percent-placeholder';
+import { scssPropertyRules } from './scss-rules/property';
+import { scssSelectorRules } from './scss-rules/selector';
 
 /**
  * SCSS 配置 = Base 配置 + SCSS 规则 + 关闭冲突规则
@@ -66,10 +40,7 @@ import { scssGeneralRules } from './scss-rules/general';
 export const scss: Config = {
   ...base,
   customSyntax: 'postcss-scss',
-  plugins: [
-    ...(Array.isArray(base.plugins) ? base.plugins : base.plugins ? [base.plugins] : []),
-    'stylelint-scss',
-  ],
+  plugins: [...(base.plugins as string[]), 'stylelint-scss'],
   rules: {
     ...base.rules,
 
