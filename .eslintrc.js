3dc474dc93edafb673f927f918bb24747c729156
高艳兵 <yanbing.gao@aliyun.com>
2025-12-08 10:28:15 +0800

    chore: 基础的配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,17 @@
 module.exports = {
   root: true,
+  ignorePatterns: [
+    '.eslintrc.js',
+    '.eslintrc.cjs',
+    '.eslintrc.mjs',
+    '.stylelintrc.js',
+    '.stylelintrc.cjs',
+    '.stylelintrc.json',
+    '.prettierrc.js',
+    '.prettierrc.cjs',
+    '.prettierrc.json',
+    '.lintstagedrc.js',
+    '.lintstagedrc.json',
+    '.editorconfig',
+  ],
 };
-
