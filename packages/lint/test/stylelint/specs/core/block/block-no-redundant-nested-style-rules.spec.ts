ca8bcdfb3416649c0406ed1852788a87023f0416
高艳兵 <yanbing.gao@aliyun.com>
2025-12-19 18:15:04 +0800

    test(stylelint): 添加 block-no-redundant-nested-style-rules 规则测试

--- packages/lint/test/stylelint/specs/core/block/block-no-redundant-nested-style-rules.spec.ts
+++ packages/lint/test/stylelint/specs/core/block/block-no-redundant-nested-style-rules.spec.ts
@@ -3,7 +3,7 @@ import { describe, expect, it } from 'vitest';
 import { resolveFixture, runStylelintWithConfig } from '../../../stylelintTestUtils';
 
 describe('block-no-redundant-nested-style-rules', () => {
-  it('应该通过使用非冗余嵌套的代码', async () => {
+  it('应该通过没有冗余嵌套的代码', async () => {
     const file = resolveFixture('core', 'block', 'block-no-redundant-nested-style-rules.css');
 
     const { errored, results } = await runStylelintWithConfig({
@@ -11,11 +11,13 @@ describe('block-no-redundant-nested-style-rules', () => {
       files: file,
     });
 
-    expect(errored).toBe(false);
-    expect(results[0]?.warnings.length).toBe(0);
+    const warnings = results[0]?.warnings ?? [];
+    const ruleWarnings = warnings.filter(w => w.rule === 'block-no-redundant-nested-style-rules');
+
+    expect(ruleWarnings.length).toBe(0);
   });
 
-  it('应该报告使用冗余嵌套的错误', async () => {
+  it('应该报告冗余嵌套的错误', async () => {
     const file = resolveFixture(
       'core',
       'block',
