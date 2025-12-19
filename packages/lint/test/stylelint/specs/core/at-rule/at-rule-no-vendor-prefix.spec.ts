63b72b9263636f4341be71d0017a2ae9e79ac28b
高艳兵 <yanbing.gao@aliyun.com>
2025-12-19 18:15:31 +0800

    test(stylelint): 添加 at-rule-no-vendor-prefix 规则测试

--- packages/lint/test/stylelint/specs/core/at-rule/at-rule-no-vendor-prefix.spec.ts
+++ packages/lint/test/stylelint/specs/core/at-rule/at-rule-no-vendor-prefix.spec.ts
@@ -3,7 +3,7 @@ import { describe, expect, it } from 'vitest';
 import { resolveFixture, runStylelintWithConfig } from '../../../stylelintTestUtils';
 
 describe('at-rule-no-vendor-prefix', () => {
-  it('应该通过使用标准 @规则的代码', async () => {
+  it('应该通过 @规则没有使用厂商前缀的代码', async () => {
     const file = resolveFixture('core', 'at-rule', 'at-rule-no-vendor-prefix.css');
 
     const { errored, results } = await runStylelintWithConfig({
@@ -11,11 +11,13 @@ describe('at-rule-no-vendor-prefix', () => {
       files: file,
     });
 
-    expect(errored).toBe(false);
-    expect(results[0]?.warnings.length).toBe(0);
+    const warnings = results[0]?.warnings ?? [];
+    const ruleWarnings = warnings.filter(w => w.rule === 'at-rule-no-vendor-prefix');
+
+    expect(ruleWarnings.length).toBe(0);
   });
 
-  it('应该报告使用厂商前缀 @规则的错误', async () => {
+  it('应该报告 @规则使用厂商前缀的错误', async () => {
     const file = resolveFixture('core', 'at-rule', 'at-rule-no-vendor-prefix-invalid.css');
 
     const { errored, results } = await runStylelintWithConfig({
