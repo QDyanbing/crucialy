bf9f1bb0371d92f96cbe5820afd8ef42677d22b6
高艳兵 <yanbing.gao@aliyun.com>
2025-12-19 18:15:28 +0800

    test(stylelint): 添加 at-rule-no-deprecated 规则测试

--- packages/lint/test/stylelint/specs/core/at-rule/at-rule-no-deprecated.spec.ts
+++ packages/lint/test/stylelint/specs/core/at-rule/at-rule-no-deprecated.spec.ts
@@ -3,7 +3,7 @@ import { describe, expect, it } from 'vitest';
 import { resolveFixture, runStylelintWithConfig } from '../../../stylelintTestUtils';
 
 describe('at-rule-no-deprecated', () => {
-  it('应该通过使用标准 @规则的代码', async () => {
+  it('应该通过没有使用已弃用 @规则的代码', async () => {
     const file = resolveFixture('core', 'at-rule', 'at-rule-no-deprecated.css');
 
     const { errored, results } = await runStylelintWithConfig({
@@ -11,8 +11,10 @@ describe('at-rule-no-deprecated', () => {
       files: file,
     });
 
-    expect(errored).toBe(false);
-    expect(results[0]?.warnings.length).toBe(0);
+    const warnings = results[0]?.warnings ?? [];
+    const ruleWarnings = warnings.filter(w => w.rule === 'at-rule-no-deprecated');
+
+    expect(ruleWarnings.length).toBe(0);
   });
 
   it('应该报告使用已弃用 @规则的错误', async () => {
