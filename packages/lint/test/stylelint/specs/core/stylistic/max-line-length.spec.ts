cd449f7f9a19ade8b83c942e8a1001481562d1d9
高艳兵 <yanbing.gao@aliyun.com>
2026-01-01 18:26:34 +0800

    test(stylelint): 优化 max-line-length 规则测试用例

--- packages/lint/test/stylelint/specs/core/stylistic/max-line-length.spec.ts
+++ packages/lint/test/stylelint/specs/core/stylistic/max-line-length.spec.ts
@@ -27,8 +27,6 @@ describe('@stylistic/max-line-length', () => {
       files: file,
     });
 
-    expect(errored).toBe(true);
-
     const warnings = results[0]?.warnings ?? [];
     const ruleWarnings = warnings.filter(w => w.rule === '@stylistic/max-line-length');
 
