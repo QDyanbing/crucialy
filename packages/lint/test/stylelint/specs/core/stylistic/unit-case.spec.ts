bedc94616cdd8c226fa7706f8c254b8c1ddcb4de
高艳兵 <yanbing.gao@aliyun.com>
2026-01-01 18:25:45 +0800

    test(stylelint): 优化 unit-case 规则测试用例

--- packages/lint/test/stylelint/specs/core/stylistic/unit-case.spec.ts
+++ packages/lint/test/stylelint/specs/core/stylistic/unit-case.spec.ts
@@ -27,8 +27,6 @@ describe('@stylistic/unit-case', () => {
       files: file,
     });
 
-    expect(errored).toBe(true);
-
     const warnings = results[0]?.warnings ?? [];
     const ruleWarnings = warnings.filter(w => w.rule === '@stylistic/unit-case');
 
