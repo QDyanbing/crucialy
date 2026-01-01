e44668cf9190a027c86a15b7bb8984ac02883fa8
高艳兵 <yanbing.gao@aliyun.com>
2026-01-01 18:28:13 +0800

    test(stylelint): 优化 selector-pseudo-element-case 规则测试用例

--- packages/lint/test/stylelint/specs/core/stylistic/selector-pseudo-element-case.spec.ts
+++ packages/lint/test/stylelint/specs/core/stylistic/selector-pseudo-element-case.spec.ts
@@ -27,8 +27,6 @@ describe('@stylistic/selector-pseudo-element-case', () => {
       files: file,
     });
 
-    expect(errored).toBe(true);
-
     const warnings = results[0]?.warnings ?? [];
     const ruleWarnings = warnings.filter(w => w.rule === '@stylistic/selector-pseudo-element-case');
 
