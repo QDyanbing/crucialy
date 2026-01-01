c3ec7ec8fc49f26e4c0096fb8ed1328c48a9132f
高艳兵 <yanbing.gao@aliyun.com>
2026-01-01 18:28:18 +0800

    test(stylelint): 优化 no-eol-whitespace 规则测试用例

--- packages/lint/test/stylelint/specs/core/stylistic/no-eol-whitespace.spec.ts
+++ packages/lint/test/stylelint/specs/core/stylistic/no-eol-whitespace.spec.ts
@@ -27,8 +27,6 @@ describe('@stylistic/no-eol-whitespace', () => {
       files: file,
     });
 
-    expect(errored).toBe(true);
-
     const warnings = results[0]?.warnings ?? [];
     const ruleWarnings = warnings.filter(w => w.rule === '@stylistic/no-eol-whitespace');
 
