ceb47157bfe4bf1d7e3756550d5cbfadb4ef202f
高艳兵 <yanbing.gao@aliyun.com>
2026-01-01 19:06:20 +0800

    test(stylelint): 优化 declaration-colon-space-before 规则测试用例

--- packages/lint/test/stylelint/specs/core/stylistic/declaration-colon-space-before.spec.ts
+++ packages/lint/test/stylelint/specs/core/stylistic/declaration-colon-space-before.spec.ts
@@ -29,8 +29,6 @@ describe('@stylistic/declaration-colon-space-before', () => {
       files: file,
     });
 
-    expect(errored).toBe(true);
-
     const warnings = results[0]?.warnings ?? [];
     const ruleWarnings = warnings.filter(
       w => w.rule === '@stylistic/declaration-colon-space-before',
