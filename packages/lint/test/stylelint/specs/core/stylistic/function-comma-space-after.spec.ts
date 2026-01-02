9ea5d630b4b4d2b764ce9029e3d2c5b59953ed2f
高艳兵 <yanbing.gao@aliyun.com>
2026-01-02 20:17:20 +0800

    test(stylelint): 优化 function-comma-space-after 规则测试用例

--- packages/lint/test/stylelint/specs/core/stylistic/function-comma-space-after.spec.ts
+++ packages/lint/test/stylelint/specs/core/stylistic/function-comma-space-after.spec.ts
@@ -27,8 +27,6 @@ describe('@stylistic/function-comma-space-after', () => {
       files: file,
     });
 
-    expect(errored).toBe(true);
-
     const warnings = results[0]?.warnings ?? [];
     const ruleWarnings = warnings.filter(w => w.rule === '@stylistic/function-comma-space-after');
 
