28a2bc9bd491723bd5ce0e45ee2f925f730b2538
高艳兵 <yanbing.gao@aliyun.com>
2026-01-02 20:17:33 +0800

    test(stylelint): 优化 at-rule-name-space-after 规则测试用例

--- packages/lint/test/stylelint/specs/core/stylistic/at-rule-name-space-after.spec.ts
+++ packages/lint/test/stylelint/specs/core/stylistic/at-rule-name-space-after.spec.ts
@@ -27,8 +27,6 @@ describe('@stylistic/at-rule-name-space-after', () => {
       files: file,
     });
 
-    expect(errored).toBe(true);
-
     const warnings = results[0]?.warnings ?? [];
     const ruleWarnings = warnings.filter(w => w.rule === '@stylistic/at-rule-name-space-after');
 
