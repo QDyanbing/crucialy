8a94c0da171cc29a60a04510e2956da011f89bea
高艳兵 <yanbing.gao@aliyun.com>
2026-01-04 18:30:06 +0800

    test(stylelint): 优化 nesting-selector-no-missing-scoping-root 规则测试用例

--- packages/lint/test/stylelint/specs/core/nesting/nesting-selector-no-missing-scoping-root.spec.ts
+++ packages/lint/test/stylelint/specs/core/nesting/nesting-selector-no-missing-scoping-root.spec.ts
@@ -16,6 +16,7 @@ describe('nesting-selector-no-missing-scoping-root', () => {
       w => w.rule === 'nesting-selector-no-missing-scoping-root',
     );
 
+    // 正向测试用例文件可能有其他规则的警告，但不应该有此规则的警告
     expect(ruleWarnings.length).toBe(0);
     expect(results[0]?.source).toBe(file);
   });
