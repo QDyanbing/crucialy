6ba9bdd5c5e22635c5b3bf8a9ff4a7f81477852b
高艳兵 <yanbing.gao@aliyun.com>
2026-01-06 14:40:49 +0800

    test(stylelint): 优化 property-no-deprecated 规则测试用例

--- packages/lint/test/stylelint/specs/core/property/property-no-deprecated.spec.ts
+++ packages/lint/test/stylelint/specs/core/property/property-no-deprecated.spec.ts
@@ -14,6 +14,7 @@ describe('property-no-deprecated', () => {
     const warnings = results[0]?.warnings ?? [];
     const ruleWarnings = warnings.filter(w => w.rule === 'property-no-deprecated');
 
+    // 正向测试用例文件可能有其他规则的警告，但不应该有此规则的警告
     expect(ruleWarnings.length).toBe(0);
     expect(results[0]?.source).toBe(file);
   });
