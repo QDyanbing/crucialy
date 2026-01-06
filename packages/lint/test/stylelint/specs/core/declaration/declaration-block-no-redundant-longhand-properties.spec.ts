31198a3e3aa10c54be80cb51a2142d41eac29113
高艳兵 <yanbing.gao@aliyun.com>
2026-01-06 14:42:03 +0800

    test(stylelint): 优化 declaration-block-no-redundant-longhand-properties 规则测试用例

--- packages/lint/test/stylelint/specs/core/declaration/declaration-block-no-redundant-longhand-properties.spec.ts
+++ packages/lint/test/stylelint/specs/core/declaration/declaration-block-no-redundant-longhand-properties.spec.ts
@@ -20,6 +20,7 @@ describe('declaration-block-no-redundant-longhand-properties', () => {
       w => w.rule === 'declaration-block-no-redundant-longhand-properties',
     );
 
+    // 正向测试用例文件可能有其他规则的警告，但不应该有此规则的警告
     expect(ruleWarnings.length).toBe(0);
     expect(results[0]?.source).toBe(file);
   });
