5d8a98052583c03910b69c58bed53d15f3960fa4
高艳兵 <yanbing.gao@aliyun.com>
2026-01-06 14:42:34 +0800

    test(stylelint): 优化 declaration-no-important 规则测试用例

--- packages/lint/test/stylelint/specs/core/declaration/declaration-no-important.spec.ts
+++ packages/lint/test/stylelint/specs/core/declaration/declaration-no-important.spec.ts
@@ -14,6 +14,7 @@ describe('declaration-no-important', () => {
     const warnings = results[0]?.warnings ?? [];
     const ruleWarnings = warnings.filter(w => w.rule === 'declaration-no-important');
 
+    // 正向测试用例文件可能有其他规则的警告，但不应该有此规则的警告
     expect(ruleWarnings.length).toBe(0);
     expect(results[0]?.source).toBe(file);
   });
