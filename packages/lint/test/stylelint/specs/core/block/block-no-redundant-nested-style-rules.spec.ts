704ed231fd41ff5f05685d07b1becda8b4e0242a
高艳兵 <yanbing.gao@aliyun.com>
2026-01-07 14:49:41 +0800

    test(stylelint): 优化 block-no-redundant-nested-style-rules 规则测试用例

--- packages/lint/test/stylelint/specs/core/block/block-no-redundant-nested-style-rules.spec.ts
+++ packages/lint/test/stylelint/specs/core/block/block-no-redundant-nested-style-rules.spec.ts
@@ -14,6 +14,7 @@ describe('block-no-redundant-nested-style-rules', () => {
     const warnings = results[0]?.warnings ?? [];
     const ruleWarnings = warnings.filter(w => w.rule === 'block-no-redundant-nested-style-rules');
 
+    // 正向测试用例文件可能有其他规则的警告，但不应该有此规则的警告
     expect(ruleWarnings.length).toBe(0);
     expect(results[0]?.source).toBe(file);
   });
