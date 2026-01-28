12a69d3b51b974a277a74c105583b5de61d22188
高艳兵 <yanbing.gao@aliyun.com>
2026-01-28 11:46:40 +0800

    test(stylelint): 优化 declaration-block-single-line-max-declarations 规则测试用例

--- packages/lint/test/stylelint/specs/core/declaration/declaration-block-single-line-max-declarations.spec.ts
+++ packages/lint/test/stylelint/specs/core/declaration/declaration-block-single-line-max-declarations.spec.ts
@@ -22,6 +22,7 @@ describe('declaration-block-single-line-max-declarations', () => {
 
     // 正向测试用例文件可能有其他规则的警告，但不应该有此规则的警告
     expect(ruleWarnings.length).toBe(0);
+    expect(errored).toBe(false);
     expect(results[0]?.source).toBe(file);
   });
 
