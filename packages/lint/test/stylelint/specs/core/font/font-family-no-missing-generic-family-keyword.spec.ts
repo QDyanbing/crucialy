9adfa77d2c0f1a3d71f6d7847106dfef76060d09
高艳兵 <yanbing.gao@aliyun.com>
2026-01-30 16:52:18 +0800

    test(stylelint): 优化 font-family-no-missing-generic-family-keyword 规则测试用例

--- packages/lint/test/stylelint/specs/core/font/font-family-no-missing-generic-family-keyword.spec.ts
+++ packages/lint/test/stylelint/specs/core/font/font-family-no-missing-generic-family-keyword.spec.ts
@@ -22,6 +22,7 @@ describe('font-family-no-missing-generic-family-keyword', () => {
 
     // 正向测试用例文件可能有其他规则的警告，但不应该有此规则的警告
     expect(ruleWarnings.length).toBe(0);
+    expect(errored).toBe(false);
     expect(results[0]?.source).toBe(file);
   });
 
