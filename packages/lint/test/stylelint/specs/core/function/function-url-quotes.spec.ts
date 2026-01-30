2e25f76a5eee2446ac7aa5e607de90563a8d2184
高艳兵 <yanbing.gao@aliyun.com>
2026-01-30 16:56:26 +0800

    test(stylelint): 优化 function-url-quotes 规则测试用例

--- packages/lint/test/stylelint/specs/core/function/function-url-quotes.spec.ts
+++ packages/lint/test/stylelint/specs/core/function/function-url-quotes.spec.ts
@@ -16,6 +16,7 @@ describe('function-url-quotes', () => {
 
     // 正向测试用例文件可能有其他规则的警告，但不应该有此规则的警告
     expect(ruleWarnings.length).toBe(0);
+    expect(errored).toBe(false);
     expect(results[0]?.source).toBe(file);
   });
 
