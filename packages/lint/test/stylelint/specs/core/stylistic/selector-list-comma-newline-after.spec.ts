a695ae290751c018f01cc7351a794233587c1843
高艳兵 <yanbing.gao@aliyun.com>
2026-01-31 17:00:54 +0800

    test(stylelint): 优化 @stylistic/selector-list-comma-newline-after 规则测试用例

--- packages/lint/test/stylelint/specs/core/stylistic/selector-list-comma-newline-after.spec.ts
+++ packages/lint/test/stylelint/specs/core/stylistic/selector-list-comma-newline-after.spec.ts
@@ -18,6 +18,7 @@ describe('@stylistic/selector-list-comma-newline-after', () => {
 
     // 正向测试用例文件可能有其他规则的警告，但不应该有此规则的警告
     expect(ruleWarnings.length).toBe(0);
+    expect(errored).toBe(false);
     expect(results[0]?.source).toBe(file);
   });
 
