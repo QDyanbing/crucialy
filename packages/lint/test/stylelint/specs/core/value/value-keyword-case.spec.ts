30f25943615f9ce62dd2aa79fc4cd4a871c217d9
高艳兵 <yanbing.gao@aliyun.com>
2026-02-01 16:08:52 +0800

    test(stylelint): 优化 value-keyword-case 规则测试用例

--- packages/lint/test/stylelint/specs/core/value/value-keyword-case.spec.ts
+++ packages/lint/test/stylelint/specs/core/value/value-keyword-case.spec.ts
@@ -1,4 +1,5 @@
 import core from '@/stylelint/core';
+import valueRules from '@/stylelint/core/value';
 import { describe, expect, it } from 'vitest';
 import { resolveFixture, runStylelintWithConfig } from '../../../stylelintTestUtils';
 
@@ -7,7 +8,7 @@ describe('value-keyword-case', () => {
     const file = resolveFixture('core', 'value', 'value-keyword-case.css');
 
     const { errored, results } = await runStylelintWithConfig({
-      config: core,
+      config: { rules: valueRules },
       files: file,
     });
 
@@ -16,6 +17,7 @@ describe('value-keyword-case', () => {
 
     // 正向测试用例文件可能有其他规则的警告，但不应该有此规则的警告
     expect(ruleWarnings.length).toBe(0);
+    expect(errored).toBe(false);
     expect(results[0]?.source).toBe(file);
   });
 
