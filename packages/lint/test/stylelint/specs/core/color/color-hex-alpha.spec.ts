5531695dc17b30f204156ddc1860a56aad1bb764
高艳兵 <yanbing.gao@aliyun.com>
2026-02-01 16:08:48 +0800

    test(stylelint): 优化 color-hex-alpha 规则测试用例

--- packages/lint/test/stylelint/specs/core/color/color-hex-alpha.spec.ts
+++ packages/lint/test/stylelint/specs/core/color/color-hex-alpha.spec.ts
@@ -1,4 +1,4 @@
-import core from '@/stylelint/core';
+import colorRules from '@/stylelint/core/color';
 import { describe, expect, it } from 'vitest';
 import { resolveFixture, runStylelintWithConfig } from '../../../stylelintTestUtils';
 
@@ -7,7 +7,7 @@ describe('color-hex-alpha', () => {
     const file = resolveFixture('core', 'color', 'color-hex-alpha.css');
 
     const { errored, results } = await runStylelintWithConfig({
-      config: core,
+      config: { rules: colorRules },
       files: file,
     });
 
@@ -16,6 +16,7 @@ describe('color-hex-alpha', () => {
 
     // 正向测试用例文件可能有其他规则的警告，但不应该有此规则的警告
     expect(ruleWarnings.length).toBe(0);
+    expect(errored).toBe(false);
     expect(results[0]?.source).toBe(file);
   });
 });
