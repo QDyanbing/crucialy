a1d00f8b50697538792dc14713cd7b1673c2ebec
高艳兵 <yanbing.gao@aliyun.com>
2026-02-02 13:32:19 +0800

    test(stylelint): 正向用例增加 expect(errored).toBe(false) - media-feature-name-value-no-unknown

--- packages/lint/test/stylelint/specs/core/media/media-feature-name-value-no-unknown.spec.ts
+++ packages/lint/test/stylelint/specs/core/media/media-feature-name-value-no-unknown.spec.ts
@@ -1,4 +1,4 @@
-import core from '@/stylelint/core';
+import mediaRules from '@/stylelint/core/media';
 import { describe, expect, it } from 'vitest';
 import { resolveFixture, runStylelintWithConfig } from '../../../stylelintTestUtils';
 
@@ -7,7 +7,11 @@ describe('media-feature-name-value-no-unknown', () => {
     const file = resolveFixture('core', 'media', 'media-feature-name-value-no-unknown.css');
 
     const { errored, results } = await runStylelintWithConfig({
-      config: core,
+      config: {
+        rules: {
+          'media-feature-name-value-no-unknown': mediaRules['media-feature-name-value-no-unknown'],
+        },
+      },
       files: file,
     });
 
@@ -16,6 +20,7 @@ describe('media-feature-name-value-no-unknown', () => {
 
     // 正向测试用例文件可能有其他规则的警告，但不应该有此规则的警告
     expect(ruleWarnings.length).toBe(0);
+    expect(errored).toBe(false);
     expect(results[0]?.source).toBe(file);
   });
 
@@ -23,7 +28,7 @@ describe('media-feature-name-value-no-unknown', () => {
     const file = resolveFixture('core', 'media', 'media-feature-name-value-no-unknown-invalid.css');
 
     const { errored, results } = await runStylelintWithConfig({
-      config: core,
+      config: { rules: mediaRules },
       files: file,
     });
 
