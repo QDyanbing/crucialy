1ef9036d8e43fa7bf07e4a5fb97a72b9269e7bb6
高艳兵 <yanbing.gao@aliyun.com>
2026-02-03 15:46:15 +0800

    test(stylelint): 正向用例增加 expect(errored).toBe(false) - length-zero-no-unit

--- packages/lint/test/stylelint/specs/core/length/length-zero-no-unit.spec.ts
+++ packages/lint/test/stylelint/specs/core/length/length-zero-no-unit.spec.ts
@@ -1,4 +1,4 @@
-import core from '@/stylelint/core';
+import lengthRules from '@/stylelint/core/length';
 import { describe, expect, it } from 'vitest';
 import { resolveFixture, runStylelintWithConfig } from '../../../stylelintTestUtils';
 
@@ -7,7 +7,9 @@ describe('length-zero-no-unit', () => {
     const file = resolveFixture('core', 'length', 'length-zero-no-unit.css');
 
     const { errored, results } = await runStylelintWithConfig({
-      config: core,
+      config: {
+        rules: { 'length-zero-no-unit': lengthRules['length-zero-no-unit'] },
+      },
       files: file,
     });
 
@@ -16,6 +18,7 @@ describe('length-zero-no-unit', () => {
 
     // 正向测试用例文件可能有其他规则的警告，但不应该有此规则的警告
     expect(ruleWarnings.length).toBe(0);
+    expect(errored).toBe(false);
     expect(results[0]?.source).toBe(file);
   });
 
@@ -23,7 +26,7 @@ describe('length-zero-no-unit', () => {
     const file = resolveFixture('core', 'length', 'length-zero-no-unit-invalid.css');
 
     const { errored, results } = await runStylelintWithConfig({
-      config: core,
+      config: { rules: lengthRules },
       files: file,
     });
 
