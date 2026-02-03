04227fea585d2f898df41a770764dada24197071
高艳兵 <yanbing.gao@aliyun.com>
2026-02-03 15:46:03 +0800

    test(stylelint): 正向用例增加 expect(errored).toBe(false) - function-no-unknown

--- packages/lint/test/stylelint/specs/core/function/function-no-unknown.spec.ts
+++ packages/lint/test/stylelint/specs/core/function/function-no-unknown.spec.ts
@@ -1,4 +1,4 @@
-import core from '@/stylelint/core';
+import functionRules from '@/stylelint/core/function';
 import { describe, expect, it } from 'vitest';
 import { resolveFixture, runStylelintWithConfig } from '../../../stylelintTestUtils';
 
@@ -7,7 +7,9 @@ describe('function-no-unknown', () => {
     const file = resolveFixture('core', 'function', 'function-no-unknown.css');
 
     const { errored, results } = await runStylelintWithConfig({
-      config: core,
+      config: {
+        rules: { 'function-no-unknown': functionRules['function-no-unknown'] },
+      },
       files: file,
     });
 
@@ -16,6 +18,7 @@ describe('function-no-unknown', () => {
 
     // 正向测试用例文件可能有其他规则的警告，但不应该有此规则的警告
     expect(ruleWarnings.length).toBe(0);
+    expect(errored).toBe(false);
     expect(results[0]?.source).toBe(file);
   });
 
@@ -23,7 +26,7 @@ describe('function-no-unknown', () => {
     const file = resolveFixture('core', 'function', 'function-no-unknown-invalid.css');
 
     const { errored, results } = await runStylelintWithConfig({
-      config: core,
+      config: { rules: functionRules },
       files: file,
     });
 
